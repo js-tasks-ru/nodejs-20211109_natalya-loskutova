a7b1c94678110a2cb1157afe228163178a1b57be
natalialoskutova <n.loskutova@just-work.org>
2021-11-15 03:56:15 +0300

    resolved summ issue

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,12 @@
+function isNumber(x) {
+  return (typeof x === 'number') && !isNaN(x);
+}
+
 function sum(a, b) {
-  /* ваш код */
+  if (!isNumber(a) || !isNumber(b)) {
+    throw new TypeError();
+  }
+  return a + b;
 }
 
 module.exports = sum;
